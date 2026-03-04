00af042bf94a996b1f1bbd1a64c8a8c2945342b0
Jonah Snider <jonah@jonahsnider.com>
2026-03-04 08:02:20 -0800

    Fix URL for dashshund cask

--- Casks/dashshund.rb
+++ Casks/dashshund.rb
@@ -2,8 +2,7 @@ cask "dashshund" do
   version "2026.0.4"
   sha256 "540a33bcf373fb4aed408dbad8dc687525af9fc74ecb5dbb080106606ea95fbc"
 
-  url "https://github.com/jonahsnider/dashshund/releases/download/v#{version}/Dashshund-#{version}-arm64.dmg",
-      verified: "github.com/jonahsnider/dashshund/"
+  url "https://github.com/jonahsnider/dashshund/releases/download/v#{version}/Dashshund-#{version}-arm64.dmg"
   name "Dashshund"
   desc "FRC dashboard for viewing camera streams"
   homepage "https://github.com/jonahsnider/dashshund"
