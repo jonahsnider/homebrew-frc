553d086732c2d66cbf6ef800adec62ab2c99d745
Jonah Snider <jonah@jonahsnider.com>
2026-06-24 16:12:38 -0700

    Update first-driver-station URL
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- Casks/first-driver-station.rb
+++ Casks/first-driver-station.rb
@@ -2,7 +2,7 @@ cask "first-driver-station" do
   version "2027.0.0-alpha-4"
   sha256 "ea0952180e577ee888c65a58dfa04d1aa63c1085a90a0b11f79fa77f6cd060e9"
 
-  url "https://github.com/wpilibsuite/FirstDriverStation-Public/releases/download/v#{version}/FirstDriverStation-macOS-universal-#{version}.pkg"
+  url "https://github.com/wpilibsuite/FirstDriverStation-Public/releases/download/v#{version}/FirstDriverStation-macOS-#{version}.pkg"
   name "FIRST Driver Station"
   desc "Driver station for the FIRST Robotics Competition"
   homepage "https://github.com/wpilibsuite/FirstDriverStation-Public"
