ea5fc1e7f45a301a1143f297e5ed906738797aa1
Jonah Snider <jonah@jonahsnider.com>
2026-07-06 13:52:02 -0700

    Fix Limelight cask checksum resolution on Linux

--- Casks/limelight-hardware-manager.rb
+++ Casks/limelight-hardware-manager.rb
@@ -2,8 +2,10 @@ cask "limelight-hardware-manager" do
   arch arm: "AppleSilicon", intel: "Intel"
 
   version "2.0.10"
-  sha256 arm:   "23d208cf628d67b26b0426ecb720a659caf867c6116d2f192462006d0406d79b",
-         intel: "b1b7304fd71142768c5206057881784f017e40cb1ca2b2512224d3df601ae76b"
+  sha256 arm:          "23d208cf628d67b26b0426ecb720a659caf867c6116d2f192462006d0406d79b",
+         intel:        "b1b7304fd71142768c5206057881784f017e40cb1ca2b2512224d3df601ae76b",
+         x86_64_linux: "b1b7304fd71142768c5206057881784f017e40cb1ca2b2512224d3df601ae76b",
+         arm64_linux:  "23d208cf628d67b26b0426ecb720a659caf867c6116d2f192462006d0406d79b"
 
   url "https://downloads.limelightvision.io/software/LimelightHardwareManager-macOS-#{arch}#{version.dots_to_underscores}.dmg"
   name "Limelight Hardware Manager"
