93510efdcd658a993a813bf1f5f8ed022b2c729e
Jonah Snider <jonah@jonahsnider.com>
2026-07-06 22:23:20 +0000

    choreo-cli: add 2026.0.3 bottle.

--- Formula/choreo-cli.rb
+++ Formula/choreo-cli.rb
@@ -10,6 +10,12 @@ class ChoreoCli < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/jonahsnider/homebrew-frc/releases/download/choreo-cli-2026.0.3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "cbe155c1afe254f6a79d9cd9e00bfb7064752e0bdef3cc9bfd4a07c70cabddaa"
+    sha256 cellar: :any,                 x86_64_linux: "1913d919216a675f95b019854007f3a2809b8844dca98260e1ea0d8bfa8ede04"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
