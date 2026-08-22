2355dd1bc9d48337e91e1e1481e74dc82efdd64c
Anka <runner@sjc20-cw712-855956b1-4f2c-4697-aeb7-3b654e68dcb0-1AB7A565117A.local>
2026-08-22 01:08:55 +0000

    rev-hardware-client 1.4.0

--- Casks/rev-hardware-client.rb
+++ Casks/rev-hardware-client.rb
@@ -1,9 +1,9 @@
 cask "rev-hardware-client" do
   arch arm: "aarch64", intel: "amd64"
 
-  version "1.0.7"
-  sha256 arm:   "9e1239fe777501c136c6c5a76e7108c3c3ad95d238be6a19c874949da4a43af9",
-         intel: "6af0bca2afbef9f6aa4022c4f03b006251a3df09b761006140c751f9d1c9c547"
+  version "1.4.0"
+  sha256 arm:   "5d2dcf1c4ec5230c5edeb953aba8fae682aa9a6a23a68af7383faec7904362f9",
+         intel: "2750affca3170e69d6de37f638e13741b466d966cc819dc265e80fceb25c6968"
 
   url "https://rhc2.revrobotics.com/download/rev-hardware-client-#{version}-mac-#{arch}.zip"
   name "REV Hardware Client 2"
