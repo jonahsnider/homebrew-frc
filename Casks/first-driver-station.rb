e4eb790916b5e8a38c8ef3cd8ebee2278f4a1d5c
Anka <runner@sat12-bq161-55b5be6b-5463-4f88-9269-48d954f19ea0-3E4B26A4F41A.local>
2026-09-02 03:00:05 +0000

    first-driver-station 2027.0.0-alpha-7

--- Casks/first-driver-station.rb
+++ Casks/first-driver-station.rb
@@ -1,10 +1,10 @@
 cask "first-driver-station" do
   arch arm: "arm64", intel: "x64"
 
-  version "2027.0.0-alpha-6"
+  version "2027.0.0-alpha-7"
 
   on_macos do
-    sha256 "2034467bc198d8c20c01fd74b8556cf6f813785c75c15f6398e3def60adcc883"
+    sha256 "5dba7a37f6022b8e9146b0e5a541334fe8474fd41deb924c8dfa80590f6cedec"
 
     url "https://github.com/wpilibsuite/FirstDriverStation-Public/releases/download/v#{version}/FirstDriverStation-macOS-#{version}.pkg"
   end
@@ -16,8 +16,8 @@ cask "first-driver-station" do
     zap trash: "~/Library/Preferences/org.wpilib.firstdriverstation.plist"
   end
   on_linux do
-    sha256 arm64_linux:  "715db59ba6386cb167fb8c0cf9b5e10e3fc61fb26a29c8b307d95f8e127d9e37",
-           x86_64_linux: "69e2015bd382c4a9264d998ca7a45a4d73b2a917fe11192beec6234fafcdbff6"
+    sha256 arm64_linux:  "0f11f558db3a5c92c73e0d07075a5a3fcfb4c3cfa5a64f988fc0416570c7937d",
+           x86_64_linux: "70a9eef1915ef10a85114d8305e94b026b1de95bd85e06f82dffd0186fb9146c"
 
     url "https://github.com/wpilibsuite/FirstDriverStation-Public/releases/download/v#{version}/FirstDriverStation-linux-#{arch}-#{version}.tar.gz"
   end
