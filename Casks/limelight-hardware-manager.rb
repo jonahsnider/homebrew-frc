d4b5719f6657e597dfdd6b7d6e6cfda5c8d4f5d7
Jonah Snider <jonah@jonahsnider.com>
2026-09-12 21:41:46 -0700

    Add Linux support to Limelight Hardware Manager

--- Casks/limelight-hardware-manager.rb
+++ Casks/limelight-hardware-manager.rb
@@ -1,14 +1,27 @@
 cask "limelight-hardware-manager" do
-  arch arm: "AppleSilicon", intel: "Intel"
-
   version "2.0.11"
+  sha256 arm:          "0044e230b2c4a6fb3baa16583a2dc9e8313af23305006e3f1c807825610ed489",
+         intel:        "43c3edbcf428fb2d4c0d821ebfda7370e8396fc39d643771c149f8965914bb37",
+         arm64_linux:  "eb27b3387a7d8aecddd3efc07c8507d778b90beae6e6157cd324c76c5109cdb5",
+         x86_64_linux: "14e277cf380fd577f949d4d352cd3983c9469dc967baa7e62cf350ffdb4fde23"
 
   on_macos do
-    sha256 arm:   "0044e230b2c4a6fb3baa16583a2dc9e8313af23305006e3f1c807825610ed489",
-           intel: "43c3edbcf428fb2d4c0d821ebfda7370e8396fc39d643771c149f8965914bb37"
+    arch arm: "AppleSilicon", intel: "Intel"
+
+    url "https://downloads.limelightvision.io/software/LimelightHardwareManager-macOS-#{arch}#{version.dots_to_underscores}.dmg"
+
+    app "Limelight Hardware Manager.app"
+
+    zap trash: "~/Library/Preferences/com.limelight.hardwaremanager.plist"
+  end
+  on_linux do
+    arch arm: "aarch64", intel: "x86_64"
+
+    url "https://downloads.limelightvision.io/software/LimelightHardwareManager-#{arch}_#{version.dots_to_underscores}.AppImage.zip"
+
+    app_image "LimelightHardwareManager-#{arch}.AppImage", target: "Limelight Hardware Manager.AppImage"
   end
 
-  url "https://downloads.limelightvision.io/software/LimelightHardwareManager-macOS-#{arch}#{version.dots_to_underscores}.dmg"
   name "Limelight Hardware Manager"
   desc "Flash and find Limelight devices"
   homepage "https://limelightvision.io/"
@@ -33,10 +46,4 @@ cask "limelight-hardware-manager" do
       versions.uniq
     end
   end
-
-  depends_on :macos
-
-  app "Limelight Hardware Manager.app"
-
-  zap trash: "~/Library/Preferences/com.limelight.hardwaremanager.plist"
 end
